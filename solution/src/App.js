94b1f83fc3d90f7892159eadc34497bf33987316
rolandin <roli.niubo@gmail.com>
2024-03-14 00:17:40 -0400

    adding comments

--- solution/src/App.js
+++ solution/src/App.js
@@ -3,6 +3,10 @@ import FormComponent from "./FormComponent/FormComponent";
 
 function App() {
   return (
+    /* 
+      I'm using Tailwindcss to style the Form, but it can be done so many ways,
+      using a component library, using a css solution such as bootstrap, styled components, emotion, tailwindcss, etc
+    */
     <div className='w-3/4 mx-auto'>
       <FormComponent />
     </div>
